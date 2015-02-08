c82afb5c8912709d535d3cb5b69c8dbabe992782
isdsc <isdsc@users.noreply.github.com>
2015-02-07 20:43:37 -0500

    Plot 2 completed

---
+++ plot2.R
@@ -0,0 +1,38 @@
+# Set the working directory
+setwd("~/Coursera/Data Science Specialization/4. Exploratory Data Analysis/ExData_Plotting1")
+
+# Load the entire data from the text file into R
+data_file <- "./data/household_power_consumption.txt"
+# Declare the variable types to speed up the data load Do not convert the date
+# strings to one of R's date classes because we don't need them for any
+# analytic work, and this will save us some time while reading the data into R
+col_types <- c("character", "character", "numeric", "numeric", "numeric",
+               "numeric", "numeric", "numeric", "numeric")
+# Use delimited format, modify some of the options to speed up the data load
+full_data <- read.csv(data_file, sep = ";", na.strings = "?", colClasses =
+                      col_types, nrows = 2.1e6, stringsAsFactors = FALSE,
+                      comment.char = "" )
+
+# Select the study period, since we haven't converted the dates into a date
+# class, use regex to allow for slight variations in the date string
+study_data_select <- grepl("^0?[12]/0?2/2007", full_data$Date)
+study_data <- full_data[study_data_select, ]
+
+# Use Lubridate package for easy handling of dates
+library(lubridate)
+study_data$timestamps <- dmy_hms(paste(study_data$Date, study_data$Time))
+
+# Set the output device to the destination file in PNG format
+png(filename = "./plot2.png", width = 480, height = 480, bg = "transparent")
+
+# Build the line plot
+plot(
+  study_data$timestamps,
+  study_data$Global_active_power,
+  xlab = "",
+  ylab = "Global Active Power (kilowatts)",
+  type = "l"
+)
+
+# Close the output device
+dev.off()
